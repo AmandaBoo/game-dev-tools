655e30bef727daf82cb1debf17bea0d72f3d9220
Amanda <amandabui1590@gmail.com>
2025-08-22 14:42:04 -0500

    fix github pages build

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,29 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? '/game-dev-tools' : '',
-  assetPrefix: isProd ? '/game-dev-tools' : '',
-  images: { 
-    unoptimized: true 
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
